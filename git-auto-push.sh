fb6b0273102f1ef8f2be6dedf14dc551ab9d1c75
Siddham Gaonkar <sidgaonkar@gmail.com>
2025-06-24 16:46:54 +0530

    Modified: git-auto-push.sh,

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -2,12 +2,24 @@
 # git-auto-push.sh: Automatically add, commit, and push all changes
 
 if [ -z "$1" ]; then
-    # Auto-generate commit message based on changed files
-    changed_files=$(git status --porcelain | awk '{print $2}' | xargs)
-    if [ -z "$changed_files" ]; then
+    status=$(git status --porcelain)
+    added=$(echo "$status" | awk '/^A /{print $2}')
+    modified=$(echo "$status" | awk '/^ M|^M /{print $2}')
+    deleted=$(echo "$status" | awk '/^ D|^D /{print $2}')
+    msg_parts=()
+    if [ -n "$added" ]; then
+        msg_parts+=("Added: $(echo $added | tr '\n' ', ' | sed 's/, $//')")
+    fi
+    if [ -n "$modified" ]; then
+        msg_parts+=("Modified: $(echo $modified | tr '\n' ', ' | sed 's/, $//')")
+    fi
+    if [ -n "$deleted" ]; then
+        msg_parts+=("Deleted: $(echo $deleted | tr '\n' ', ' | sed 's/, $//')")
+    fi
+    if [ ${#msg_parts[@]} -eq 0 ]; then
         commit_msg="Update: no file changes detected"
     else
-        commit_msg="Update: modified $(echo $changed_files | tr ' ' ', ')"
+        commit_msg="$(IFS='; '; echo "${msg_parts[*]}")"
     fi
 else
     commit_msg="$1"
