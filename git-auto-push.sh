61d615f52921ffd04609b6ceafa014fdf6129d00
Siddham Gaonkar <sidgaonkar@gmail.com>
2025-06-24 16:37:26 +0530

    Update: modified git-auto-push.sh

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -2,7 +2,13 @@
 # git-auto-push.sh: Automatically add, commit, and push all changes
 
 if [ -z "$1" ]; then
-    read -p "Enter commit message: " commit_msg
+    # Auto-generate commit message based on changed files
+    changed_files=$(git status --porcelain | awk '{print $2}' | xargs)
+    if [ -z "$changed_files" ]; then
+        commit_msg="Update: no file changes detected"
+    else
+        commit_msg="Update: modified $(echo $changed_files | tr ' ' ', ')"
+    fi
 else
     commit_msg="$1"
 fi
