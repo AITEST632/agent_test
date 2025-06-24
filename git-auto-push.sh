e11e209606831d970b3692992bdc092637ec1728
Siddham Gaonkar <sidgaonkar@gmail.com>
2025-06-24 15:24:06 +0530

    Update: dynamic commit message for auto-push script

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # git-auto-push.sh: Automatically add, commit, and push all changes
 
+if [ -z "$1" ]; then
+    read -p "Enter commit message: " commit_msg
+else
+    commit_msg="$1"
+fi
+
 git add .
-git commit -m "Auto: update project"
+git commit -m "$commit_msg"
 git push
