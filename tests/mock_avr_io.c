7ad0ae41f326d5a0c4b1852c472107aaa700aa35
Siddham Gaonkar <sidgaonkar@gmail.com>
2025-06-26 09:59:11 +0530

    Added: tests/mock_avr_io.c tests/test_app_main.c tests/test_app_main.exe,;Modified: tests/Makefile,;Deleted: tests/test_led_hal.exe,

---
+++ tests/mock_avr_io.c
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+// Mock hardware registers as global variables
+uint8_t PORTB = 0;
+uint8_t DDRB = 0;
---
+++ tests/test_app_main.c
@@ -0,0 +1,33 @@
+#include "unity.h"
+#include "mock_avr_io.h"
+#include "../inc/app/app_main.h"
+#include "../inc/hal/led_hal.h"
+#include "../inc/services/delay_service.h"
+
+void setUp(void) {
+    PORTB = 0;
+    DDRB = 0;
+}
+void tearDown(void) {}
+
+void test_app_main_should_initialize_led_and_blink(void)
+{
+    led_init();
+    // After led_init, DDRB bit 0 should be set
+    TEST_ASSERT_BITS_HIGH(1 << 0, DDRB);
+
+    led_on();
+    // After led_on, PORTB bit 0 should be set
+    TEST_ASSERT_BITS_HIGH(1 << 0, PORTB);
+
+    led_off();
+    // After led_off, PORTB bit 0 should be cleared
+    TEST_ASSERT_BITS_LOW(1 << 0, PORTB);
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_app_main_should_initialize_led_and_blink);
+    return UNITY_END();
+}
